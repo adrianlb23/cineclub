4f9d259105b8d05b9f75b375245e6466bfebfe96
adrian <alopezbanobre@gmail.com>
2024-10-02 22:16:52 -0300

    minor changes

--- app.js
+++ app.js
@@ -10,3 +10,13 @@ btnregistro.addEventListener('click', () => {
 btnlogin.addEventListener('click', () => {
     container.classList.remove("active");
 });
+
+/* Botón no funciona
+
+// Botón "Log" redirecciona a página de login (index -> login)
+const logButton = document.getElementById("logButton");
+logButton.addEventListener("click", function() {
+    window.location.href = "login.html";
+});
+
+*/
